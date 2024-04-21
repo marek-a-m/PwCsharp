5448e99ef2a353c1b07e36dbd62d34434909468d
Marek Misiak <misiak.marek@gmail.com>
2024-04-21 12:25:49 +0200

    pierwsze zajêcia

---
+++ wstep/wstep/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Prośba o podanie prędkości w km/h
+        Console.WriteLine("Podaj prędkość w kilometrach na godzinę:");
+        double kmh = Convert.ToDouble(Console.ReadLine());
+
+        // Przeliczenie prędkości na m/s
+        double ms = kmh * 1000 / 3600;
+        string formattedMs = ms.ToString("0.##");
+
+        // Wyświetlenie wyniku
+        Console.WriteLine($"Prędkość w metrach na sekundę: {formattedMs} m/s");
+    }
+}
